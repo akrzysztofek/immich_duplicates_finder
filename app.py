d7412f1e5c35660645e0a3778b2aaac9bd3fef65
CoveTar <covetar@samale.ca>
2024-12-30 03:33:20 -0500

    multiproccessing

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import os
+import multiprocessing
 
 from api import fetchAssets
 from db import startup_db_configurations, startup_processed_assets_db, startup_processed_duplicate_faiss_db
 from startup import startup_sidebar
-from imageDuplicate import generate_db_duplicate,show_duplicate_photos_faiss,calculateFaissIndex
-
+from imageDuplicate import generate_db_duplicate, show_duplicate_photos_faiss, calculateFaissIndex
 
 # Set the environment variable to allow multiple OpenMP libraries
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 ###############STARTUP#####################
 
 # Set page title and favicon
-st.set_page_config(page_title="Immich duplicator finder ", page_icon="https://immich.app/img/immich-logo-stacked-dark.svg")
+st.set_page_config(page_title="Immich duplicator finder", page_icon="https://immich.app/img/immich-logo-stacked-dark.svg")
 
 startup_db_configurations()
 startup_processed_assets_db()
@@ -33,8 +33,8 @@ def setup_session_state():
         'avoid_thumbnail_jpeg': True,
         'is_trashed': False,
         'is_favorite': True,
-        'stop_process' : False,
-        'stop_index' : False,
+        'stop_process': False,
+        'stop_index': False,
         'photo_choice': 'Thumbnail (fast)'  # Initialize with default action to not show duplicates
     }
     for key, default_value in session_defaults.items():
@@ -90,36 +90,43 @@ def configure_sidebar():
         additional_data = "Immich duplicator finder"
         st.markdown(f"**Version:** {program_version}\n\n{additional_data}")
 
+def fetch_assets(immich_server_url, api_key, timeout):
+    return fetchAssets(immich_server_url, api_key, timeout, 'IMAGE')
+
+def calculate_faiss_index(assets, immich_server_url, api_key):
+    calculateFaissIndex(assets, immich_server_url, api_key)
+
+def generate_duplicate_db():
+    generate_db_duplicate()
+
 def main():
-    #print(fetchAssets(immich_server_url, api_key,timeout, 'VIDEO'))
     setup_session_state()
     configure_sidebar()
     assets = None
 
     # Attempt to fetch assets if any asset-related operation is to be performed
     if st.session_state['calculate_faiss'] or st.session_state['generate_db_duplicate'] or st.session_state['show_faiss_duplicate']:
-        assets = fetchAssets(immich_server_url, api_key,timeout, 'IMAGE')
+        with multiprocessing.Pool() as pool:
+            assets_result = pool.apply_async(fetch_assets, (immich_server_url, api_key, timeout))
+            assets = assets_result.get()
         if not assets:
             st.error("No assets found or failed to fetch assets.")
             return  # Stop further execution since there are no assets to process
 
-
     # Calculate the FAISS index if the corresponding flag is set
     if st.session_state['calculate_faiss'] and assets:
-        calculateFaissIndex(
-            assets, 
-            immich_server_url, 
-            api_key
-        )
+        with multiprocessing.Pool() as pool:
+            pool.apply_async(calculate_faiss_index, (assets, immich_server_url, api_key))
 
     # Show FAISS duplicate photos if the corresponding flag is set
     if st.session_state['generate_db_duplicate']:
-        generate_db_duplicate()
+        with multiprocessing.Pool() as pool:
+            pool.apply_async(generate_duplicate_db)
 
     # Show FAISS duplicate photos if the corresponding flag is set
     if st.session_state['show_faiss_duplicate'] and assets:
         show_duplicate_photos_faiss(
-            assets, st.session_state['limit'], 
+            assets, st.session_state['limit'],
             st.session_state['faiss_min_threshold'],
             st.session_state['faiss_max_threshold'],
             immich_server_url,
